d75dcec1fbd2822aa0c01657ad07cbbe9fb35f23
Jeff Forcier <jeff@bitprophet.org>
2011-10-12 01:14:49 +0000

    Big overhaul to use custom logic objects

--- haproxy.py
+++ haproxy.py
@@ -6,7 +6,63 @@ from collections import defaultdict
 HAPROXY_STAT_SOCKET = '/var/run/haproxy.sock'
 
 
-def socket_to_dicts(socket_path):
+class HaproxyStatusEntry(object):
+    """
+    Encapsulates some basic logic re: Haproxy status CSV lines.
+
+    E.g. whether a line refers to a backend/frontend, one of the actual backend
+    servers being monitored, etc. Many fields' meanings then derive from that
+    root observation.
+
+    Behaves like the wrapped dict except for the extra methods/attributes.
+    """
+    def __init__(self, mapping):
+        self.mapping = mapping
+
+    NAME_MAP = {
+        'name': 'svname',
+        'proxy': 'pxname',
+        'active': 'act',
+    }
+
+    def __getattr__(self, name):
+        # Prefer, in order:
+        # * NAME_MAP => mapping keys
+        # * Actual mapping keys
+        # * Mapping object attributes
+        try:
+            return self.mapping[self.NAME_MAP.get(name, name)]
+        except KeyError:
+            return getattr(self.mapping, name)
+
+    def __getitem__(self, key):
+        return self.mapping[key]
+
+    def __repr__(self):
+        return repr(self.mapping)
+
+    def __str__(self):
+        return str(self.mapping)
+
+    @property
+    def is_server(self):
+        """
+        Is this object representing a proxied server?
+        """
+        return self.name not in ('BACKEND', 'FRONTEND')
+
+    @property
+    def is_active(self):
+        """
+        Object is a server and is active ('act' == '1').
+        """
+        return self.is_server and self.active == '1'
+
+
+def get_entries(socket_path):
+    """
+    Obtain status CSV from Haproxy socket, return HaproxyStatusEntry objects.
+    """
     # Obtain CSV data from UNIX socket
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect(socket_path)
@@ -27,15 +83,16 @@ def socket_to_dicts(socket_path):
     dictified = csv.DictReader(result.splitlines())
 
     # Clean trailing empty-field pair (good job haproxy? kind of strange.)
+    # and turn into custom object
     cleaned = []
     for entry in dictified:
         del entry['']
-        cleaned.append(entry)
+        cleaned.append(HaproxyStatusEntry(entry))
 
     return cleaned
 
 
-def statuses(dicts):
+def statuses(entries):
     """
     Return mapping of the form $map['proxy']['server'] = 'STATUS'.
 
@@ -43,8 +100,8 @@ def statuses(dicts):
     """
     # 2-level dict
     processes = defaultdict(dict)
-    for d in dicts:
-        processes[d['pxname']][d['svname']] = d['status']
+    for e in entries:
+        processes[e.proxy][e.name] = e.status
     # Cast back to vanilla dict for shits, giggles and better pprinting.
     # (Not expecting clients to modify.)
     return dict(processes)
@@ -52,6 +109,8 @@ def statuses(dicts):
 
 if __name__ == "__main__":
     import pprint
-    dicts = socket_to_dicts(HAPROXY_STAT_SOCKET)
-    pprint.pprint(dicts)
-    pprint.pprint(statuses(dicts))
+    entries = get_entries(HAPROXY_STAT_SOCKET)
+    pprint.pprint(statuses(entries))
+    print "Active statuses:"
+    for entry in filter(lambda x: x.is_server, entries):
+        print "%s => %s: %s" % (entry.proxy, entry.name, entry.is_active)
